703dda0437efcb4925c484f5bd9dfd7c71c19e4c
Caio <caio.teixeira@arapiraca.ufal.br>
2024-01-16 23:38:41 -0300

    Add file listing by index in the 'bases' folder
    and added dimension "calculator"

--- src/file_converter.py
+++ src/file_converter.py
@@ -1,12 +1,13 @@
 class Converter:
-    def __init__(self, file, length) -> None:
-        self.length = length
+    def __init__(self, file) -> None:
         with open(file, 'r') as arquivo:
-            self.lista = arquivo.read()
+            linhas = arquivo.readlines()
+            self.lista = ' '.join(linhas)
+            self.length = len(linhas[0].split())
 
     def matrix_converter(self):
         grafo = [float(i) for i in self.lista.split()]
-        grafo = [grafo[i:i + int(self.length)] for i in range(0, len(grafo), int(self.length))]
+        grafo = [grafo[i:i + self.length] for i in range(0, len(grafo), self.length)]
         return grafo
 
     def grafo(self):
--- src/main.py
+++ src/main.py
@@ -1,7 +1,32 @@
+from pathlib import Path
 import file_converter
-import PCV
 
-file = file_converter.Converter(input('File name: '), input('Dimension: ')).grafo()
-#retorno = PCV.resolver_tsp(file)
+def list_files_in_directory(directory):
+    files = list(directory.glob('*'))
+    files_dict = {i: file.name for i, file in enumerate(files, start=1)}
+    return files_dict
 
-print(file)
+def main():
+    src_dir = Path(__file__).resolve().parent / 'bases'
+    files_dict = list_files_in_directory(src_dir)
+
+    for index, file in files_dict.items():
+        print(f"{index}: {file}")
+
+    try:
+        file_index = int(input('Selecione o índice do arquivo: '))
+        file_name = files_dict[file_index]
+        file_path = src_dir / file_name
+    except (ValueError, KeyError):
+        print("Índice inválido.")
+        return
+
+    if file_path.exists() and file_path.is_file():
+        graph = file_converter.Converter(str(file_path)).grafo()
+        for key, value in graph.items():
+            print(f"{key}: {value}")
+    else:
+        print(f"Arquivo '{file_name}' não encontrado no diretório '{src_dir}'.")
+
+if __name__ == "__main__":
+    main()
