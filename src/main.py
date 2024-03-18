c7b4c5283ad969b2d9942540cf8d0da0fa5bccd1
Caio <caio.teixeira@arapiraca.ufal.br>
2024-03-17 23:05:10 -0300

    algo prim

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 from pathlib import Path
 import file_converter
+from algo.prim import prim
 
 def list_files_in_directory(directory):
     files = list(directory.glob('*'))
     files_dict = {i: file.name for i, file in enumerate(files, start=1)}
     return files_dict
+
+def choose_algorithm():
+    print("Escolha o algoritmo:")
+    print("1: Prim")
+    print("2: Dijkstra")
+    print("3: Christofides")
+    print("4: Força Bruta")
+    alg_choice = input("Digite o número do algoritmo: ")
+    return alg_choice
 
 def main():
     src_dir = Path(__file__).resolve().parent / 'bases'
@@ -15,16 +25,33 @@ def main():
 
     try:
         file_index = int(input('Selecione o índice do arquivo: '))
-        file_name = files_dict[file_index]
-        file_path = src_dir / file_name
-    except (ValueError, KeyError):
+        file_name = files_dict.get(file_index)
+    except ValueError:
+        print("Entrada inválida. Por favor, digite um número.")
+        return
+
+    if not file_name:
         print("Índice inválido.")
         return
 
+    file_path = src_dir / file_name
+
     if file_path.exists() and file_path.is_file():
         graph = file_converter.Converter(str(file_path)).grafo()
-        for key, value in graph.items():
-            print(f"{key}: {value}")
+
+        alg_choice = choose_algorithm()
+        print(40*'=')
+
+        if alg_choice == '1':
+            prim(graph)
+        elif alg_choice == '2':
+            pass
+        elif alg_choice == '3':
+            pass
+        elif alg_choice == '4':
+            pass
+        else:
+            print("Escolha de algoritmo inválida.")
     else:
         print(f"Arquivo '{file_name}' não encontrado no diretório '{src_dir}'.")
 
